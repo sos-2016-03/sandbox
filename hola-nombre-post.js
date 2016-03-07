e63ca2450bb79cd4d630db3c6e310d91d63cf0a1
patricia28 <patrigonzalez28@gmail.com>
2016-03-07 10:58:40 +0100

    app hola nombre post. Closes #9

--- hola-nombre-post.js
+++ hola-nombre-post.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var nombre="Patricia";
 
-var port = (process.env.PORT || 13000);
+var port = (process.env.PORT || 15000);
 
 app.post("/",(req,res)=>{
 	res.write("Hola " + nombre);
