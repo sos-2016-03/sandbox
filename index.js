ba33e76273bfa3fbeedf3941e4180e573eb920c6
patricia28 <patrigonzalez28@gmail.com>
2016-05-15 13:31:02 +0200

    API SLA

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("hello world");
-console.log("How are you?");
+var express = require("express");
+var bodyParser = require("body-parser");
+var governify = require("governify");
+
+var app = express();
+governify.control(app,{
+  datastore :"http://datastore.governify.io/api/v6.1/",
+  namespace :"sos-2016-03-group",
+  defaultPath: "/group"
+});
+var port = (process.env.PORT || 12000);
+
+app.use(bodyParser.json());
+
+app.get("/group/names", (req, res)=>{
+  res.send([
+    {name: "Patricia González Sevilla"},
+    {name: "Ana Salas Urbano"},
+    {name: "ALberto Jesús Rodríguez Pulido"}
+    ])
+})
+
+app.listen(port, ()=>{
+	console.log("Listening on port: " + port);
+})
